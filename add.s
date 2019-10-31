700f8fe7662959b0a848916232357b66b0d19d10
Shannon Skipper <shannonskipper@gmail.com>
2019-10-30 22:37:13 -0700

    Remove unnecessary step

--- add.s
+++ add.s
@@ -4,8 +4,7 @@
 _add:
         push    rbp             ; store stack from pointer
         mov     rax, rdi        ; get first argument
-        mov     rdx, rsi        ; get second argument
-        add     rax, rdx        ; add second argument to first
+        add     rax, rsi        ; add second argument to first
         pop     rbp             ; restore base pointer
 
         ret                     ; return from procedure
