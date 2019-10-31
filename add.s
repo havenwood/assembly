c9c3d043daaaa99bd99c05f9b5e14187de30a4eb
Shannon Skipper <shannonskipper@gmail.com>
2019-10-30 17:27:18 -0700

    Skip the stack frame

--- add.s
+++ add.s
@@ -2,10 +2,8 @@
 
         section .text
 _add:
-        push    rbp             ; create stack frame
         mov     eax, edi        ; get first argument
         mov     edx, esi        ; get second argument
         add     eax, esi        ; add second argument to first
-        pop     rbp             ; restore base pointer
 
         ret                     ; return from procedure
