b100cfbcf398802d5fcf486d9f14776466daff77
Shannon Skipper <shannonskipper@gmail.com>
2019-10-30 21:32:49 -0700

    Revert "Skip the stack frame"
    This reverts commit c9c3d043daaaa99bd99c05f9b5e14187de30a4eb.

--- add.s
+++ add.s
@@ -2,8 +2,10 @@
 
         section .text
 _add:
+        push    rbp             ; create stack frame
         mov     eax, edi        ; get first argument
         mov     edx, esi        ; get second argument
         add     eax, esi        ; add second argument to first
+        pop     rbp             ; restore base pointer
 
         ret                     ; return from procedure
